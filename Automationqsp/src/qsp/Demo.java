e7db7165621c1bb325b521051fee486a70c28bbd
HP <HP@DESKTOP-2GD2IB2>
2021-09-17 09:31:07 +0530

    update1

--- Automationqsp/src/qsp/Demo.java
+++ Automationqsp/src/qsp/Demo.java
@@ -3,7 +3,7 @@ package qsp;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("hihihihi");
+		System.out.println("hihihihi1");
 		// TODO Auto-generated method stub
 
 	}
